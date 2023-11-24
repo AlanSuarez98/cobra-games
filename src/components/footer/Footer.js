4f4ebbf6b7b14adbc4bc22bf9a1d1ada5f3c1c7d
Alan Suarez <alansuarez641@hotmail.com>
2023-11-24 16:41:04 -0300

    Add new´s functions &  repair bug´s

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,9 +3,15 @@ import logo from "../../Logo.png";
 import "./Footer.css";
 import Icon from "../icon/Icon";
 import { faInstagram, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
-import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
+  const handleInstagramClick = () => {
+    window.open(`https://www.instagram.com/cobragames.ok/`, "_blank");
+  };
+
+  const handleWhatsappClick = () => {
+    window.open(`https://wa.me/543416697243`, "_blank");
+  };
   return (
     <div className="containerFooter">
       <div className="containerLogo">
@@ -15,9 +21,12 @@ const Footer = () => {
         <h1>Cobra Games</h1>
         <p>Tienda online de venta de videojuegos</p>
         <div className="socialIcon">
-          <Icon css={"iconSocial"} icon={faInstagram} />
-          <Icon css={"iconSocial"} icon={faWhatsapp} />
-          <Icon css={"iconSocial"} icon={faArrowUp} />
+          <button onClick={handleInstagramClick}>
+            <Icon css={"iconSocial"} icon={faInstagram} />
+          </button>
+          <button onClick={handleWhatsappClick}>
+            <Icon css={"iconSocial"} icon={faWhatsapp} />
+          </button>
         </div>
       </div>
     </div>
